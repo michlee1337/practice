77c4ba7befb9f52fa4478d5eeb7e140ad9ad3118
Michelle S Lee <michlee1337@gmail.com>
2018-05-20 19:57:14 +0800

    exception handling

--- Python/pythonds.py
+++ Python/pythonds.py
@@ -5,3 +5,19 @@
 
 wordlist = ['cat','dog','rabbit']
 letterlist = [ch for ch in set("".join(wordlist))]
+
+# exception handling
+#   using try and exept to do something if exception raised
+anumber = -10
+
+try:
+       print(math.sqrt(anumber))
+    except:
+       print("Bad Value for square root")
+       print("Using absolute value instead")
+       print(math.sqrt(abs(anumber)))
+#      using raise to throw your own exception
+if anumber < 0:
+    raise RuntimeError("You can't use a negative number")
+else:
+    print(math.sqrt(anumber))

43090ec8f313d365abd0f24f12f931c01040cc4f
Michelle S Lee <michlee1337@gmail.com>
2018-10-01 23:32:15 +0900

    completed basic functions

--- Python/Blackjack.py
+++ Python/Blackjack.py
@@ -8,35 +8,47 @@ class card:
         self.suit = suit
 
 class player:
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
         self.cards = []
-
-hi = card(7,'heart')
+        self.total = 0
 
 # create deck
 deck = []
 for i in ['diamonds','clubs','hearts','spades']:
-    for j in range(13):
+    for j in range(1,14):
         deck.append(card(j,i))
-print(deck[1].suit)
 
 # shuffle function
 from random import shuffle
 shuffle(deck)
-print(deck[1].suit)
 
 # draw card function
-def draw(player):
-    player.cards.append(deck.pop())
+def draw(p1):
+    card = deck.pop()
+    p1.cards.append(card)
+    p1.total += card.number
 
 # deal function
-def deal(players,deck):
-    for i in players():
-        draw(players[i])
-        draw(players[i])
-
-# stay function
+def deal(pList):
+    for i in pList:
+        draw(i)
+        draw(i)
 
 # check who won function
+def whoWon(ps):
+    dummy = player('dumdum')
+    winner = dummy
+    for i in ps:
+        print(winner.name)
+        if i.total > winner.total:
+            winner = i
+    print(winner.name)
+    return(winner)
+Ann = player('Ann')
+Bnn = player('Bnn')
+deal([Ann,Bnn])
+print(Ann.total,Bnn.total)
+whoWon([Ann,Bnn])
 
 # main code for gameplay

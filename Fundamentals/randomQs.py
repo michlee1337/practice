830ec3a7ffe876e115fa26d696177da0c9227d2c
Michelle S Lee <michlee1337@gmail.com>
2018-11-24 10:33:26 +0900

    WIP finding first common parent in tree

--- Fundamentals/randomQs.py
+++ Fundamentals/randomQs.py
@@ -62,7 +62,112 @@ def hToD(num):
         multiple += 1
     return(ret_int)
 
+# balanced brackets: check if brackets are balanced in a given string
 
+def balBrack(str):
+    brack_stack = []
+    for c in str:
+        if c == '(':
+            brack_stack.append(1)
+        elif c == ')':
+            if brack_stack == []:
+                return('you have an extra closed bracket')
+            brack_stack.pop()
+    if brack_stack == []:
+        return('looking good :)')
+    else:
+        return('you have an extra open bracket')
+
+# print missing numbers in array:
+def printMissing(arr):
+    miss_int = []
+    for i in range(len(max(arr))):
+        if arr.index(i) is None:
+            miss_int.append(i)
+    return(miss_int)
+
+def isAnagram(str):
+    mid = floor(len(str)/2)
+    for i in range(mid-1):
+        if str[i] != str[len(str)-i]:
+            return(False)
+    return(True)
+
+# find the first common parent of two nodes in the tree
+class Tree():
+    def __init__(self):
+        self.root = None
+    def insert(self,val):
+        pass
+    def commParent(self,val1,val2):
+        loc = self._find(val1)
+        if loc is None:
+            return('not in tree')
+        while loc is not None:
+            try1 = self.bfs(loc,val2)
+            # should remove checked nodes
+            if try1 is not None:
+                return(loc.parent())
+            parent = loc.parent()
+            if parent.left() == loc:
+                loc = parent.right()
+            else:
+                loc = parent.left()
+                # oops infinite
+                # should probably do this recursively w a modified dfs
+
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Tree():
+    def __init__(self):
+        self.root = None
+
+    def _insert(cur_node, val):
+        if cur_node is None:
+            cur_node.val = val
+            return(0)
+        if val < cur_node.val:
+            self._insert(cur_node.left, val)
+        if val > cur_node.val:
+            self._insert(cur_node.right, val)
+
+    def insert(self,val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self._insert(self.root, val)
+
+    def BFS(self,val):
+        search_stack = []
+        pass
+
+    def commParent(self,val1,val2):
+        loc = self._find(val1)
+        if loc is None:
+            return('not in tree')
+        while loc is not None:
+            try1 = self.bfs(loc,val2)
+            # should remove checked nodes
+            if try1 is not None:
+                return(loc.parent())
+            parent = loc.parent()
+            if parent.left() == loc:
+                loc = parent.right()
+            else:
+                loc = parent.left()
+                # oops infinite
+                # should probably do this recursively w a modified dfs
+
+
+# merge k sorted lists
+
+# binary search trees
+
+# learn search and sort algos
 
 if __name__ == "__main__":
     print('testing rev')
@@ -81,7 +186,12 @@ if __name__ == "__main__":
     print(bToD([1,0,1,0,1,1,1,0]))
     print(hToD([1,'D',8,'A']))
     print(hToD([4,'F']))
+    print('testing brackets')
+    print(balBrack('()'))
+    print(balBrack('('))
+    print(balBrack(')'))
+    print(balBrack('((erer)r)'))
 
 
-
+# interval scheduling
 #Find local maxima in array

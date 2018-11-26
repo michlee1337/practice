48b82ff4cc0782e1de651418cb55fd7d77295bec
Michelle S Lee <michlee1337@gmail.com>
2018-11-26 21:16:53 +0900

    completed interval scheduling w greedy algo

--- Python/intervalScheduling.py
+++ Python/intervalScheduling.py
@@ -17,8 +17,37 @@ def intervalScheduling(times, interval):
         interval -= longest
     return(executed, interval, times)
 
+def intScheduling(meetings, interval):
+    executed = []
+    while len(meetings) > 0:
+        start_times = []
+        end_times = []
 
-print(intervalScheduling([8,4,2,1],15))
+        for times in meetings:
+            start_times.append(times[0])
+            end_times.append(times[1])
+
+        # take earliest ending time
+        best = end_times.index(min(end_times))
+
+        # if no meetings left possible, end
+        if meetings[best][1] > interval:
+            return(executed, interval, meetings)
+
+        # else execute
+        else:
+            executed.append(meetings[best])
+            last_time = meetings[best][1]
+            meetings.pop(best)
+
+        # remove all meetings made impossible
+        poss_meetings = meetings
+        for time in meetings:
+            if time[0] <= last_time:
+                meetings.remove(time)
+    return(executed, interval, meetings)
+
+print(intScheduling([[2,4],[8,12],[2,15]],15))
 
     # do you consider that they have to start at this time?
     # what about dependencies? if task a has to be completed before task b

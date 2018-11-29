0b2f19c11756b720d86c4a516542eb40fa62d104
Michelle S Lee <michlee1337@gmail.com>
2018-11-30 01:14:07 +0900

    started karasuba

--- Fundamentals/karatsuba.py
+++ Fundamentals/karatsuba.py
@@ -42,7 +42,6 @@ def basicMultiplication(a,b):
             res[i] += p[i]
     # check for carry values
     carry = 0
-    print(res)
     for p in range(len(res)-1,-1,-1):
         cur = res[p]
         res[p] = (cur + carry) % 10
@@ -50,4 +49,30 @@ def basicMultiplication(a,b):
     return(res)
 
 #print(basicMultiplication(9,2))
-print(basicMultiplication(3841,24165))
+#print(basicMultiplication(3841,24165))
+
+# karasuba method
+def karasuba(int1,int2):
+    # base case: if both len 1, return simple multiplication
+    int1 = [int(c) for c in str(int1)]
+    int2 = [int(c) for c in str(int2)]
+    print(int1,int2)
+    if len(int1) == 1 and len(int2) == 1:
+        return(int1[0] * int2[0])
+    # recursively iterate
+    else:
+        print(int1,int2)
+        int1_mid = len(int1)//2
+        int2_mid = len(int2)//2
+        a = int1[0:int1_mid]
+        b = int1[int1_mid:len(int1)]
+        c = int2[0:int2_mid]
+        d = int2[int2_mid:len(int2)]
+        ac = karasuba(a,c)
+        bd = karasuba(b,d)
+        abcd = karasuba([int(i) for i in str(int(''.join(map(str,a)))+int(''.join(map(str,b))))],[int(j) for j in str(int(''.join(map(str,c)))+int(''.join(map(str,d))))])
+        print(abcd)
+        return(abcd-ac-bd)
+
+
+print(karasuba(3841,24165))

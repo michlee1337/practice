024d1be435e96950f737b757f1069a03a4db63df
Michelle S Lee <michlee1337@gmail.com>
2018-11-30 18:42:18 +0900

    abstracted add helper

--- Fundamentals/karatsuba.py
+++ Fundamentals/karatsuba.py
@@ -51,28 +51,53 @@ def basicMultiplication(a,b):
 #print(basicMultiplication(9,2))
 #print(basicMultiplication(3841,24165))
 
+def addHelper(arr_of_arr):
+        # make all partials same length by padding 0s
+        standLen = max([len(i) for i in arr_of_arr])
+
+        for p in arr_of_arr:
+            for x in range(standLen - len(p)):
+                p.insert(0,0)
+
+        # add each element in partials from the last to first index
+        res = [0] * standLen
+        for i in range(standLen-1,-1,-1):
+            for p in arr_of_arr:
+                res[i] += p[i]
+        # check for carry values
+        carry = 0
+        for p in range(len(res)-1,-1,-1):
+            cur = res[p]
+            res[p] = (cur + carry) % 10
+            carry = (cur + carry) // 10
+        return(res)
+
+print(addHelper([[3,2],[9,5,3,2],[4,6]]))
+
 # karasuba method
 def karasuba(int1,int2):
     # base case: if both len 1, return simple multiplication
-    int1 = [int(c) for c in str(int1)]
-    int2 = [int(c) for c in str(int2)]
-    print(int1,int2)
+    print('each',int1,int2)
+    if len(int1) == 0 or len(int2) == 0:
+        return([0])
     if len(int1) == 1 and len(int2) == 1:
-        return(int1[0] * int2[0])
+        print(int1,int2)
+        return([int1[0] * int2[0]])
     # recursively iterate
     else:
-        print(int1,int2)
         int1_mid = len(int1)//2
         int2_mid = len(int2)//2
         a = int1[0:int1_mid]
         b = int1[int1_mid:len(int1)]
         c = int2[0:int2_mid]
         d = int2[int2_mid:len(int2)]
-        ac = karasuba(a,c)
+        ac = karasuba(a,c) + (4*[0])
         bd = karasuba(b,d)
-        abcd = karasuba([int(i) for i in str(int(''.join(map(str,a)))+int(''.join(map(str,b))))],[int(j) for j in str(int(''.join(map(str,c)))+int(''.join(map(str,d))))])
-        print(abcd)
-        return(abcd-ac-bd)
+        print('res',ac,bd)
+        print('add',addHelper(ac+bd))
+        #abcd = karasuba([int(i) for i in str(int(''.join(map(str,a)))+int(''.join(map(str,b))))],[int(j) for j in str(int(''.join(map(str,c)))+int(''.join(map(str,d))))]) + (2*[0])
+        return('hi')
+        #return([abcd-ac-bd])
 
 
-print(karasuba(3841,24165))
+print(karasuba([3,8,4,1],[2,4,1,6,5]))

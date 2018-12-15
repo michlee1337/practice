68a8217a046d3e6be6000efe8fe10e4aee76bba1
Michelle S Lee <michlee1337@gmail.com>
2018-12-16 01:25:36 +0900

    changed implementation to stack w indexes rather than nodes

--- Fundamentals/stackQs2.py
+++ Fundamentals/stackQs2.py
@@ -1,18 +1,41 @@
 # implement stack through min heap priority q
 
-# define node first
-class node():
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.left = None
-        self.right = None
-
-class minHeap():
-    # heap defined by root
+class MinHeap():
     def __init__(self):
-        self.root = None
-        self.count = 0
+        self.heap = []
+
+    def parent(self,i):
+        return(self[(i-1)//2])
+
+    def leftChild(self,i):
+        return(self[(i*2)+1])
+
+    def rightChild(self,i):
+        return(self[(i*2)+2])
+
+    def _push(cur_node, x):
+        if cur_node.left is None:
+            cur_node.left = Node(self.count, x)
+            self.count += 1
+            return(0)
+        elif cur_node.right is None:
+            cur_node.right = Node(self.count, x)
+            return(0)
+        else:
+            cur_node = cur_node.left
 
     def push(self,x):
-        if
+        # if empty tree, insert at root
+        if self.root is None:
+            self.root = Node(self.count, x)
+            self.count += 1
+            return(0)
+        # find next empty slot
+        _push(self.root,x)
+        # check children
+
+
+
+
+        if self.root == None:
+            self

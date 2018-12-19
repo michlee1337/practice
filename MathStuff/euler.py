e7f1c7ef3de98811c5a09204c9ca2f39a39822fb
Michelle S Lee <michlee1337@gmail.com>
2018-12-19 12:24:00 +0900

    updated to track path

--- MathStuff/euler.py
+++ MathStuff/euler.py
@@ -11,20 +11,25 @@ def find_next(curr_point, step_size, roc_func):
     return(curr_point)
 
 # main func
-def euler_method(curr_point, step_size, roc_func, end_point):
+def euler_method(curr_point, step_size, roc_func, end_point, path):
     if curr_point[0] >= end_point:
+        path.append([curr_point[0],curr_point[1]])
         print(curr_point)
+        print(str(path).replace('[',"{").replace(']','}'))
         return(0)
     else:
-        euler_method(find_next(curr_point, step_size,roc_func),step_size,roc_func,end_point)
+        path.append([curr_point[0],curr_point[1]])
+        euler_method(find_next(curr_point, step_size,roc_func),step_size,roc_func,end_point, path)
 
 print('step size 6')
-euler_method([0,0],6,roc_money,12)
+euler_method([0,0],6,roc_money, 12, [])
 print('step size 1')
-euler_method([0,0],1,roc_money,12)
+euler_method([0,0],1,roc_money,12, [])
 print('step size 0.02')
-euler_method([0,0],0.02,roc_money,12)
+euler_method([0,0],0.02,roc_money,12, [])
 print('step size 0.015')
-euler_method([0,0],0.015,roc_money,12)
+euler_method([0,0],0.015,roc_money,12, [])
 print('step size 0.013')
-euler_method([0,0],0.013,roc_money,12)
+euler_method([0,0],0.013,roc_money,12, [])
+
+# how to plot it nicely in function

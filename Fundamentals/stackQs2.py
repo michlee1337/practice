29b689a5ee7ef6e3095468378eda42d67d629d9f
Michelle S Lee <michlee1337@gmail.com>
2018-12-21 00:37:15 +0900

    fixed bugs in min heap

--- Fundamentals/stackQs2.py
+++ Fundamentals/stackQs2.py
@@ -11,13 +11,20 @@ class MinHeap():
 
     def bubbleUp(self,i):
         # while parent exists for this index
-        while i//2 > 0:
+        print(self.heap)
+        print('cur ind',i)
+        if i <= 0:
+            return(0)
+        while i//2 >= 0:
+            print('check')
             # check with parent, if larger, switch and repeat w new index
-            if self.heap[i] > self.heap[(i-1)//2]:
+            if self.heap[i] < self.heap[(i-1)//2]:
+                print('switched')
+                print(self.heap[i], self.heap[(i-1)//2])
                 temp = self.heap[(i-1)//2]
                 self.heap[(i-1)//2] = self.heap[i]
                 self.heap[i] = temp
-                bubbleUp(self,(i-1)//2)
+                self.bubbleUp((i-1)//2)
             # else, end and return
             else:
                 return(0)
@@ -25,4 +32,21 @@ class MinHeap():
     def push(self,x):
         self.heap.append(x)
         self.size += 1
-        self.bubbleUp(self.size)
+        self.bubbleUp(self.size-1)
+print(0//2)
+if __name__ == "__main__":
+    my_heap = MinHeap()
+    my_heap.push('7')
+    print(my_heap.heap)
+    print('______')
+    my_heap.push('6')
+    print(my_heap.heap)
+    print('______')
+    my_heap.push('5')
+    print(my_heap.heap)
+    print('______')
+    my_heap.push('4')
+    print(my_heap.heap)
+    print('______')
+    my_heap.push('3')
+    print(my_heap.heap)

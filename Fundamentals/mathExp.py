a132ba50a8a572b2d0f560bc15a1196f601070ed
michlee1337 <michlee1337@gmail.com>
2019-01-09 16:40:02 +0530

    completed and tested in to post fix conversion

--- Fundamentals/mathExp.py
+++ Fundamentals/mathExp.py
@@ -2,17 +2,26 @@
 
 def inToPost():
 	expO = raw_input("input infix exp seperated by spaces: ").split()
-	operators = ['/','*','+','-']
+	operators = ['/','*','+','-','(']
 	output = []
 	opstack = []
-	#bracket = False
 
 	# for each item in input
 	for item in expO:
 
+		# if open bracket, add to opp stack
+		if item == "(":
+			opstack.append(item)
+
+		# if close bracket, pop all ops until open bracket
+		elif item == ")":
+			while opstack[-1] != "(":
+				output.append(opstack.pop())
+			opstack.pop()
+
 		# if operator, add to opstack after removing prioritised operators
-		if item in operators:
-			while opstack != [] and operators.index(opstack[0]) < operators.index(item):
+		elif item in operators:
+			while opstack != [] and operators.index(opstack[-1]) < operators.index(item):
 				output.append(opstack.pop())
 			opstack.append(item)
 
@@ -27,7 +36,7 @@ def inToPost():
 
 
 
-	print(output)
+	print(" ".join(output))
 
 if __name__ == "__main__":
 	inToPost()

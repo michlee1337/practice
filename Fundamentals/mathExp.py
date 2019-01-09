d2bb61570b68a1af1a658a80cb56455937827cc6
michlee1337 <michlee1337@gmail.com>
2019-01-09 16:23:57 +0530

    cleaned up prints

--- Fundamentals/mathExp.py
+++ Fundamentals/mathExp.py
@@ -9,21 +9,20 @@ def inToPost():
 
 	# for each item in input
 	for item in expO:
-		print('t',output)
-
-		# if operand, add to end of output
-		if item not in operators:
-			output.append(item)
 
 		# if operator, add to opstack after removing prioritised operators
-		elif item in operators:
+		if item in operators:
 			while opstack != [] and operators.index(opstack[0]) < operators.index(item):
 				output.append(opstack.pop())
 			opstack.append(item)
 
+		# if operand, add to end of output
+		else:
+			output.append(item)
+
+
 	# pop anything remaining on opstack
 	while len(opstack) != 0:
-		print(opstack)
 		output.append(opstack.pop())
 
 

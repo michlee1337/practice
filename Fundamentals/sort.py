de08ad5007a75cb85d43050ef28c4d6497040f09
michlee1337 <michlee1337@gmail.com>
2019-01-17 01:23:31 +0530

    completed and tested insertion sort

--- Fundamentals/sort.py
+++ Fundamentals/sort.py
@@ -1,3 +1,12 @@
+def insertionSort(arr):
+    for i in range(len(arr[1:])):
+        min = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min]:
+                min = j
+        arr[i],arr[min] = arr[min], arr[i]
+    return(arr)
+
 def mergeSort(arr):
     if len(arr) == 1:
         return(arr)
@@ -24,6 +33,9 @@ def mergeSort(arr):
 
         return(sorted)
 
-print(mergeSort([19,47,2792,3,4,2]))
 
-def sortK()
+if __name__ == "__main__":
+    print(insertionSort([]))
+    print(insertionSort([0]))
+    print(insertionSort([23,4,-1,0,399]))
+    #print(mergeSort([19,47,2792,3,4,2]))

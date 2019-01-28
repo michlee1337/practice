f4e0cf3f3d9ee50d88ebbe586d4446f6e99d6e34
michlee1337 <michlee1337@gmail.com>
2019-01-28 23:58:34 +0530

    changed var names slightly and created func to identify new max sum when you add one int to arr

--- Fundamentals/max_sub.py
+++ Fundamentals/max_sub.py
@@ -21,11 +21,11 @@ def maxSub_brute(arr):
 def maxSub_linear(arr):
 	# keep track of overall max 
 	maxSum_ovr = float('-inf')
-	maxInd_ovr = []
+	maxArr_ovr = []
 
 	# keep track of max ending in last index
 	maxSum_end = 0
-	maxInd_end = []
+	maxArr_end = []
 
 	# for each i in arr
 	for i in range(len(arr)):
@@ -37,23 +37,46 @@ def maxSub_linear(arr):
 
 		if arr[i] > maxSum_end:
 			maxSum_end = arr[i]
-			maxInd_end = [arr[i]]
+			maxArr_end = [arr[i]]
 
 		# or past max + current
 		else:
-			maxInd_end.append(arr[i])
+			maxArr_end.append(arr[i])
 		
 		# if larger than overall max, update
 		if maxSum_end > maxSum_ovr:
 			maxSum_ovr = maxSum_end
-			maxInd_ovr = maxInd_end
+			maxArr_ovr = maxArr_end
 
 	# checking edge case: empty array
 	if maxSum_ovr == float('-inf'):
 		maxSum_ovr = 0
 
 
-	return(maxSum_ovr, maxInd_ovr)
+	return(maxSum_ovr, maxArr_ovr)
+
+def max_addOne(x, mx):
+	# find max ending in last index
+	# keep track of running max ending in n
+	maxSum = float('-inf')
+	# keep track of sum up til current index
+	sum_curr = 0
+
+	# for each index from last to first
+	for i in range(len(x)-1,-1,-1):
+		# add new i to sum_Curr
+		sum_curr += x[i]
+
+		# if current sum more than global, update
+		if sum_curr > maxSum:
+			maxSum = sum_curr
+	# return larger of old max sum or new max sum
+	if mx > maxSum:
+		return(mx)
+	else:
+		return(maxSum)
+
+
 
 
 if __name__ == "__main__":
@@ -61,5 +84,8 @@ if __name__ == "__main__":
 	print(maxSub_linear([1,-2,3]))
 	print(maxSub_linear([]))
 	print(maxSub_linear([-2,-3,-1,-4,-6]))
+	print(max_addOne([-1,2,3,-4,5,10],6))
+	print(max_addOne([1,-2,3],1))
+	print(max_addOne([-2,-3,-1,-4,-6, -1],-1))
 
 

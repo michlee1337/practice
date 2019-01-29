e570c38814c12487d9d6b3c228ad89a2b910d830
michlee1337 <michlee1337@gmail.com>
2019-01-29 15:48:25 +0530

    working on getting closest and fartehest pair

--- Fundamentals/max_sub.py
+++ Fundamentals/max_sub.py
@@ -76,7 +76,46 @@ def max_addOne(x, mx):
 	else:
 		return(maxSum)
 
+def quickSort(arr):
+    if len(arr) <= 1:
+        return(arr)
 
+    # select last elem as pivot
+    j = len(arr)-1
+
+    # sort arr so all items less than pivot are on left of the rest
+    i = 0
+    k = j-1
+
+    while i <= k:
+        while i <= j and arr[i] <= arr[j]:
+            i += 1
+        while k >= 0  and arr[k] >= arr[j]:
+            k -= 1
+        if i < k:
+            arr[k],arr[i] = arr[i],arr[k]
+
+    # recurse on items less than and more than pivot
+    return(quickSort(arr[0:k+1]) + [arr[j]] + quickSort(arr[k+1:-1]))
+
+
+def closeFar(arr):
+	if len(arr) < 2:
+		return('less than 2 is not a pair :(')
+	arr_s = quickSort(arr)
+	far = [arr_s[0],arr_s[-1]]
+	least_dist = float('inf')
+	close = []
+	for i in range(1,len(arr_s),1):
+		if abs(arr_s[i] - arr_s[i-1]) < least_dist:
+			least_dist = arr_s[i] - arr_s[i-1]
+			close = [arr_s[i],arr_s[i-1]]
+	return(close,far)
+
+	# T(n) approx= nlgn + n (but quicksort worst case n^2)
+	# O(n) = n^2
+	# omega(n) = nlgn
+	# average case = nlgn
 
 
 if __name__ == "__main__":
@@ -87,5 +126,7 @@ if __name__ == "__main__":
 	print(max_addOne([-1,2,3,-4,5,10],6))
 	print(max_addOne([1,-2,3],1))
 	print(max_addOne([-2,-3,-1,-4,-6, -1],-1))
+	print(closeFar([4,-10,2]))
+	print(closeFar([4,-10,2,9999,-89,-90]))
 
 

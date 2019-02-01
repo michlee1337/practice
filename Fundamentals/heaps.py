2440c7f0c87cd7bb46e9f33cacac89a7e70d0472
michlee1337 <michlee1337@gmail.com>
2019-02-01 18:02:24 +0530

    max heapify

--- Fundamentals/heaps.py
+++ Fundamentals/heaps.py
@@ -1,8 +1,25 @@
-class Heap():
-    def __init__:
-        
+def max_heapify(arr,I):
+
+	# add I to the btottom of the heap
+	i = len(arr)
+	arr.append(I)
+
+	# while not at root
+	while i >= 1:
+		print(i)
+		# if I is more than parent, swap positions and update i
+		print('comparing',arr[i],arr[(i-1)//2])
+		if arr[i] > arr[(i-1)//2]:
+			arr[i],arr[(i-1)//2] = arr[(i-1)//2],arr[i]
+			i = (i-1)//2
+		# else, done
+		else:
+			break
+	return(arr)
 
 
-    # heap class
-    # changes any array into max heap
-    # can be changed into a sorted array
+if __name__ == "__main__":
+	print(max_heapify([],7))
+	print(max_heapify([10,5,3,4,1],7))
+	print(max_heapify([10,5,3,4,-9],15))
+

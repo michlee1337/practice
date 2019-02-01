3b4f9500630229da9f425c85f237454bdf0e4d96
michlee1337 <michlee1337@gmail.com>
2019-02-01 21:32:52 +0530

    floatey arrow

--- Fundamentals/heaps.py
+++ Fundamentals/heaps.py
@@ -6,9 +6,7 @@ def max_heapify(arr,I):
 
 	# while not at root
 	while i >= 1:
-		print(i)
 		# if I is more than parent, swap positions and update i
-		print('comparing',arr[i],arr[(i-1)//2])
 		if arr[i] > arr[(i-1)//2]:
 			arr[i],arr[(i-1)//2] = arr[(i-1)//2],arr[i]
 			i = (i-1)//2
@@ -17,9 +15,20 @@ def max_heapify(arr,I):
 			break
 	return(arr)
 
+def build_max_heap(arr):
+	# for each item in array, bubble up (max heapify)
+	for i in range(1,len(arr),1):
+		arr[:i+1] = max_heapify(arr[:i],arr[i])
+	return(arr)
+
 
 if __name__ == "__main__":
 	print(max_heapify([],7))
 	print(max_heapify([10,5,3,4,1],7))
 	print(max_heapify([10,5,3,4,-9],15))
+	print(build_max_heap([6,3,-9,1,20,-49]))
+	print(build_max_heap([]))
+	print(build_max_heap([9,2,6,0,1,4,1]))
+
+
 

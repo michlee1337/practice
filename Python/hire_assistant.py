2aa08f19b8ee1e87f0fbcf326e22abab5f92f10a
michlee1337 <michlee1337@gmail.com>
2019-02-07 19:46:53 +0530

    empirical vs real avg runtime analysis for hiring problem

--- Python/hire_assistant.py
+++ Python/hire_assistant.py
@@ -1,5 +1,8 @@
 #____Hire Assistant Problem____
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 # simulate one hiring period
 def sim_hiring_days(days):
@@ -25,8 +28,35 @@ def sim_hiring_periods(start,stop, step):
 		num_hired.append(sim_hiring_days(i))
 	return(num_hired)
 
-# average result
+# main code
 
+# set params
+min_period = 1
+max_period = 10000
+step_size = 100
+avg_over = 30
+
+# simulate and store results
+sim_results = []
+for i in range(avg_over):
+	sim_results.append(sim_hiring_periods(min_period,max_period, step_size))
+
+# average results
+avg_res = np.mean(sim_results,axis=0)
+
+# calculate theoretical
+x = range(min_period,max_period+1,step_size)
+y = [math.log(i) + 1 for i in x]
+
+# plot real
+plt.plot(x,avg_res,label="real")
+# plot theoretical
+plt.plot(x,y,label="theoretical")
+plt.xlabel('Length of Hiring Period (days)')
+plt.ylabel('Avg Hires (over'+str(avg_over)+')')
+plt.title('Avg Hires over Length of Hiring Period')
+plt.legend()
+plt.show()
 
 
 import unittest
@@ -35,15 +65,18 @@ class TestHeap(unittest.TestCase):
 
 	def test_sim_hiring_days(self):
 		self.assertTrue(sim_hiring_days(10) >= 0 and sim_hiring_days(10) <= 10)
-		self.assertTrue(sim_hiring_days(3) >= 0 and sim_hiring_days(10) <= 3)
+		self.assertTrue(sim_hiring_days(3) >= 0 and sim_hiring_days(3) <= 3)
 		self.assertEqual(sim_hiring_days(0),0)
 
 	def test_sim_hiring_periods(self):
-		self.assertTrue(len(sim_hiring_periods(1,10,1))==10)
-		self.assertTrue(len(sim_hiring_periods(1,15,5))==3)
+		self.assertEqual(len(sim_hiring_periods(1,10,1)),10)
+		self.assertEqual(len(sim_hiring_periods(1,15,5)),3)
 		self.assertTrue(sim_hiring_periods(1,10,1)[9]<=10)
 		self.assertTrue(sim_hiring_periods(1,15,5)[2]<=15)
 
 if __name__ == '__main__':
 	suite = unittest.TestLoader().loadTestsFromTestCase(TestHeap)
 	unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+

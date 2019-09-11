744173db8ad80c30620b1ec6d6b7a2712ffa63bb
michlee1337 <michlee1337@gmail.com>
2019-09-11 11:30:24 +0200

    cleaned up

--- techDevGuide/maxVacation.py
+++ techDevGuide/maxVacation.py
@@ -16,8 +16,6 @@ class Solution:
             Can only fly once a week (on Monday, first day of the week)
         '''
 
-        # brute force: try every possible permutation, that's O(n!) time
-
         # memo where [i][j] shows max days from i city in j+1 week
         Solution.memo = [[-1 for _ in (enumerate(days[0]))] for _ in enumerate(flights)]
 
@@ -42,26 +40,6 @@ class Solution:
                         possibleDays = days[city][week] + self._findMaxVacationDp(flights, days, i, week + 1, curDays + days[i][week])
                         Solution.memo[city][week] = max(possibleDays, Solution.memo[city][week])
             return(Solution.memo[city][week])
-
-    def _findMaxVacation(self, flights, days, city, week, curDays):
-        # base case: if week = last week, end and remember the max value of this path
-        if week == len(days[0]):
-            Solution.maxDays = max(curDays, Solution.maxDays)
-            return(0)
-        # recursive step: try all possible path
-        else:
-            for i,_ in enumerate(flights):
-                if (i == city or flights[city][i]):
-                    ####if Solution.memo != -1:
-                    self._findMaxVacation(flights, days, i, week + 1, curDays + days[i][week])
-
-        # dp
-
-        # approach: bottom up dp
-        # what matters is _what city you're in_ + _what week_
-        # 2d memoization of ^^
-
-        # best is best of best(any city, last week) + best(any reachable city from last week, this week) and best(this week, this city)
 
 if __name__=="__main__":
     flights = ast.literal_eval(sys.argv[1])

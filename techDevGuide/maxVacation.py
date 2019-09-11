11551e8945ae32eadca11f03638de3fd85f490d2
michlee1337 <michlee1337@gmail.com>
2019-09-11 11:28:31 +0200

    day 24

--- techDevGuide/maxVacation.py
+++ techDevGuide/maxVacation.py
@@ -20,15 +20,19 @@ class Solution:
 
         # memo where [i][j] shows max days from i city in j+1 week
         Solution.memo = [[-1 for _ in (enumerate(days[0]))] for _ in enumerate(flights)]
+
         for i,_ in enumerate(Solution.memo):
-            Solution.memo[i][-1] = days[i][0]
-        return(self._findMaxVacationDp(flights, days, 0, 0, 0))
+            Solution.memo[i][-1] = days[i][-1]
+        self._findMaxVacationDp(flights, days, 0, -1, 0)
+        return(max([city[0] for city in Solution.memo]))
 
     def _findMaxVacationDp(self, flights, days, city, week,curDays):
-        # if no more weeks, return curDays
-        if week == len(days[0]):
-            Solution.maxDays = curDays
-            return(curDays)
+        # if just starting, search all cities
+        if week == -1:
+            for i,_ in enumerate(flights):
+                if (i == city or flights[city][i]):
+                    possibleDays = self._findMaxVacationDp(flights, days, i, 0, 0)
+                    Solution.memo[i][0] = max(possibleDays, Solution.memo[i][0])
 
         # else return corresponding memo (fill if necessary)
         else:
@@ -66,3 +70,4 @@ if __name__=="__main__":
     print("days: ", days)
     sol = Solution()
     print(sol.maxVacation(flights, days))
+    #print(Solution.memo)

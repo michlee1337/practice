c5073ebc2a8d6a44558dd5a32d77990c01e02bbb
michlee1337 <michlee1337@gmail.com>
2020-02-27 10:14:14 -0300

    clean

--- MathStuff/search/forward_chain.py
+++ MathStuff/search/forward_chain.py
@@ -1,38 +1,28 @@
-'''
-Implement a forward chaining solver in Python for ascertaining the truth of a symbol given a knowledge base of definite clauses, following the algorithm in Fig 7.15 of Russell & Norvig. You could implement a class called Symbol to represent a logical symbol, as well as a class called DefiniteClause to represent the structure of a definite clause, with a premise set (the body of the clause) and a symbol for the conclusion (the head of the clause). A knowledge base can then be represented as a list of definite clauses. Have your code ready to paste in at the start of class.
-
-
-'''
-
 from collections import defaultdict
+import queue
+
 class DefiniteClause():
-    def __init__(self, body, head):
+    def __init__(self, body, head, inf = False):
         self.body = body
         self.head = head
 
 
 def fc(kb):
-    count = defaultdict(int)
-    inferred = defaultdict(bool)
-    agenda = []
-
+    agenda = queue.Queue()
+    exp = defaultdict(bool) # expanded, pass
     closed = [] # remember everything that is true
 
     for c in kb:
         if len(c.body) == 0:
-            agenda.insert(0,c)
-        else:
-            count[c] = len(c.body)
+            agenda.put(c.head)
 
-    while agenda:
-        p = agenda.pop()
-        closed.append(p.head)
-        #if p == q: return (True)
-        if not inferred[p]:
-            inferred[p] = True
+    while not agenda.empty():
+        p = agenda.get()
+        if not exp[p]:
+            exp[p] = True
+            closed.append(p)
             for c in kb:
-                if p.head in c.body:
-                    count[c] -= 1
-                    if count[c] == 0:
-                        agenda.insert(0,c)
+                if p in c.body: c.body.remove(p)
+                if len(c.body) == 0:
+                    agenda.put(c.head)
     return(closed)

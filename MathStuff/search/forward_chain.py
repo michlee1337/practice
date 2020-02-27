604e97f0ffffaafe6e1441b47dbf311760441c41
michlee1337 <michlee1337@gmail.com>
2020-02-27 10:24:23 -0300

    rm test

--- MathStuff/search/forward_chain.py
+++ MathStuff/search/forward_chain.py
@@ -28,16 +28,3 @@ def fc(kb):
                 if len(c.body) == 0: agenda.put(c.head) # update agenda
 
     return(closed)
-
-S1 = DefiniteClause(["B", "C"],"A")
-S2 = DefiniteClause(["D"],"B")
-S3 = DefiniteClause(["E"],"B")
-S4 = DefiniteClause(["H"],"D")
-S5 = DefiniteClause(["G","B"],"F")
-S6 = DefiniteClause(["C","K"],"G")
-S7 = DefiniteClause(["A","B"],"J")
-S8 = DefiniteClause([],"C")
-S9 = DefiniteClause([],"E")
-
-kb = [S1,S2,S3,S4,S5,S6,S7,S8,S9]
-print(fc(kb))
